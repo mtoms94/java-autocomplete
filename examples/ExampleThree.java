29e709e9a9e473440840bfeed9863fb35918bd19
mtoms94 <mdt1@williams.edu>
2020-08-03 20:46:05 -0400

    add images

---
+++ examples/ExampleThree.java
@@ -0,0 +1,16 @@
+class SomeObject {
+    SomeObject() {
+    }
+
+    public int someMethod() {
+        return 0;
+    }
+}
+
+public class ExampleThree {
+    public static void main(int[] args) {
+        SomeObject so = new SomeObject();
+        String str = "hello";
+        str.su
+    }
+}
---
+++ examples/ExampleTwo.java
@@ -0,0 +1,15 @@
+class SomeObject {
+    SomeObject() {
+    }
+
+    public int someMethod() {
+        return 0;
+    }
+}
+
+public class ExampleTwo {
+    public static void main(int[] args) {
+        SomeObject so = new SomeObject();
+        String str = new 
+    }
+}
